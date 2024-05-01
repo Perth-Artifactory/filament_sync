d196f75e6265f489a53f66b191dfa8c20451b61e
Fletcher <me@fboyd.me>
2024-05-01 11:59:27 +0800

    Better uptime check

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from processors import siddament
 from pprint import pprint
 import json
 import requests
+import sys
 
 
 # Load config
@@ -10,7 +11,15 @@ with open("config.json", "r") as f:
 
 # Check whether spoolman is up
 r = requests.get(f"{config['spoolman_url']}/info")
-print(f"Connected to spoolman, version is: {r.json()['version']}")
+if r.status_code != 200:
+    print("Failed to connect to spoolman")
+    sys.exit(1)
+spoolman_info = r.json()
+if "version" not in spoolman_info:
+    print("Failed to connect to spoolman")
+    sys.exit(1)
+
+print(f"Connected to spoolman, version is: {spoolman_info['version']}")
 
 processors = [siddament]
 
