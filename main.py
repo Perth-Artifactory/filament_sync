329afa6063b1ddd7d2a1f2b3fdce5c068b2bb8b3
Fletcher <me@fboyd.me>
2024-05-01 11:48:19 +0800

    Add connectivity check

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from processors import siddament
 from pprint import pprint
+import json
+import requests
+
+
+# Load config
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+# Check whether spoolman is up
+r = requests.get(f"{config['spoolman_url']}/info")
+print(f"Connected to spoolman, version is: {r.json()['version']}")
 
 processors = [siddament]
 
